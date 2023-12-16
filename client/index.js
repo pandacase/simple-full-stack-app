458c65f37ac1f06f40ddc3b4563ea4b09649b5d4
pandacase <2227119810@qq.com>
2023-12-16 15:00:30 +0800

    update client

---
+++ client/index.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch()
+    loadHTMLTable([]);
+});
+
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+    }
+}
+
