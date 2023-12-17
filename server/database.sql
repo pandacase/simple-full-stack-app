aa5b6a6467c9dc15b7e434420fb5c5ce2f88c828
pandacase <2227119810@qq.com>
2023-12-17 16:24:02 +0800

    Update database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,20 +1,22 @@
 
 -- create table
 create table names (
-    id int primary key,
-    name varchar(127),
-    data_added datetime
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(127),
+    date_added DATETIME
 );
 
 -- check user list
-select user from mysql.user;
+select user FROM mysql.user;
 
 -- create user
-create user admin1 identified by '1111';
+CREATE USER admin1 IDENTIFIED BY '1111';
 
 -- grant all to user
-grant select, insert, update, delete on simple_server.* to admin1;
+GRANT SELECT, INSERT, UPDATE, DELETE ON simple_server.* TO admin1;
 
 
 
-insert into names values(101, 'panda', '2023-12-16');
+INSERT INTO names VALUES(001, 'panda', '2023-12-16 00:00:00');
+INSERT INTO names VALUES(002, 'amber', '2023-12-16 18:32:32');
+INSERT INTO names VALUES(003, 'hoeny', '2023-12-16 22:15:16');
