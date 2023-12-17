1ff8abffd78335958091d8b5851dd89b612b04d9
pandacase <2227119810@qq.com>
2023-12-17 16:23:45 +0800

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,15 +1,54 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+///////////////////////////
+////// load the page //////
+///////////////////////////
+document.addEventListener('DOMContentLoaded', getAll());
+
+function getAll() {
     fetch('http://localhost:5000/getAll')
     .then(response => response.json())
     .then(data => loadHTMLTable(data['data']));
-});
+}
 
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+    } else {
+        let tableHtml = "";
+        data.forEach(function ({id, name, date_added}) {
+            tableHtml += '<tr>';
+            tableHtml += `<td>${id}</td>`;
+            tableHtml += `<td>${name}</td>`;
+            tableHtml += `<td>${formatDate(date_added)}</td>`;
+            tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;
+            tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td>`;
+            tableHtml += '</tr>';
+        });
+    
+        table.innerHTML = tableHtml;
+    }
+
+}
+
+function formatDate(dateString) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+}
+
+
+///////////////////////////
+///// Add-name botton /////
+///////////////////////////
 const addBtn = document.querySelector('#add-name-btn');
-
 addBtn.onclick = function() {
     const nameInput = document.querySelector('#name-input');
     const name = nameInput.value;
+    if (name.length === 0) {
+        return;
+    }
     nameInput.value = "";
 
     fetch('http://localhost:5000/insert', {
@@ -19,39 +58,43 @@ addBtn.onclick = function() {
         method: 'POST',
         body: JSON.stringify({name : name})
     })
-    .then(response => response.json())
-    .then(data => insertRowIntoTable(data['data']));
+    .then(response => {
+        if (!response.ok) {
+            console.log(response.status);
+        } else {
+            getAll();
+        }
+    })
 }
 
-function insertRowIntoTable(data) {
 
-}
+///////////////////////////
+///// Row edit/delete /////
+///////////////////////////
 
-function loadHTMLTable(data) {
-    const table = document.querySelector('table tbody');
+document.querySelector('table tbody').addEventListener('click', function(event) {
+    if (event.target.className === 'delete-row-btn') {
+        deleteRowById(event.target.dataset.id);
+    } else if (event.target.className === 'edit-row-btn') {
 
-    if (data.length === 0) {
-        table.innerHTML = `<tr>
-                             <td class='no-data' colspan='5'>No Data</td>
-                           </tr>`;
-    } else {
-        table.innerHTML = "";
-        data.forEach(row => {
-            const rowData = `<tr>
-                              <td>${row.id}</td>
-                              <td>${row.name}</td>
-                              <td>${formatDate(row.data_added)}</td>
-                              <td><button class='delete-row-btn'>Delete</button></td>
-                              <td><button class='edit-row-btn'>Edit</button></td>
-                            </tr>`;
-            table.insertAdjacentHTML('beforeend', rowData);
-        });
     }
+});
+
+function deleteRowById(id) {
+    fetch('http://localhost:5000/delete/' + id, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            getAll();
+        }
+    });
 }
 
 
-function formatDate(dateString) {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const date = new Date(dateString);
-    return date.toLocaleDateString(undefined, options);
-}
+
+
+
+
+
